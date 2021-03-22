7a84c909e107cf31ff8839357c141127869215a7
Uduru <kurasiki.homura@gmail.com>
2021-03-23 05:31:29 +0800

    Implement student listing

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
+/* Requirement and instances */
 const express = require("express");
 const app = express();
- 
+const bodyParser = require("body-parser");
+const fs = require("fs");
+
+/* Enable json parsing */
+app.use(express.urlencoded({extended : true}));
+app.use(express.json());
+
+/* Static path */
+app.use("/", express.static(__dirname));
+
 /* Any number from the IANA ephemeral port range (49152-65535) */
 const port = 50000;
 
@@ -9,7 +19,15 @@ app.listen(port, () => {
 });
 
 /* List Students */
-
+app.post("/list", (req, res) => {
+    fs.readFile('./students.json', (err, jsonString) => {
+        if (err) {
+            console.log("File read failed:", err);
+            return;
+        }
+        res.send(jsonString);
+    });
+});
 
 /* Search Student */
 
--- system.js
+++ system.js
@@ -1,6 +1,18 @@
-// $(document).ready() is depreciated.
+// $(document).ready() is deprecated.
 $(function(){
     console.log("Page Ready");
 
+    
+    $("#ajax-list button").on("click", (event) => {
+        /* What is this? */
+        event.preventDefault();
 
+        /* List Students */
+        $.post("./list", (data) => {
+            $("#list-body").empty();
+            $.each(JSON.parse(data), function(index, val){
+                $("#list-body").append(`${index}: ${val}<br>`);
+            });
+        });
+    });
 });
